3a8aadf11d85adf5fa334d1de08f382fa1a5a478
Manuel Henriquez <76463428+ManuelDHenriquez@users.noreply.github.com>
2023-11-26 20:46:20 -0600

    Creación de vista de categorías

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\clienteController;
+use App\Http\Controllers\categoriasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// Controladores de usuarios y clientes
 Route::get('/login', [loginController::class, 'index'])-> name('usuarios.login');
 
 Route::post('/logIn', [loginController::class, 'logIn'])-> name('usuarios.logIn');
@@ -27,3 +28,6 @@ Route::post('/logIn', [loginController::class, 'logIn'])-> name('usuarios.logIn'
 Route::get('/registroCliente', [clienteController::class, 'registrarCliente'])-> name('usuarios.registrarCliente');
 
 Route::post('/storeCliente', [clienteController::class, 'storeCliente'])-> name('usuario.store');
+
+// Controladores de categorias
+Route::get('/categorias', [categoriasController::class, 'index'])-> name('categorias.categorias');
